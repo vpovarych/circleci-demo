22b403eff3ab93711627e44176b14db45334b008
Automate 6500 <automate6500@gmail.com>
2019-02-02 18:08:47 -0800

    Add logic to check for existing version

--- upload-new-version.sh
+++ upload-new-version.sh
@@ -4,12 +4,19 @@ bucket=${2:-${application}-bundles}
 tag=$(date +%F)-$(git rev-parse --short HEAD)
 bundle=${application}-${tag}.zip
 
-git archive HEAD -o /tmp/${bundle}
+check_version=$(aws elasticbeanstalk describe-application-versions --application-name target-app --version-label $tag --query "ApplicationVersions[0]")
 
-aws s3 cp /tmp/${bundle} s3://${bucket}
+if [ "${check_version}" = "null" ];
+then
+    git archive HEAD -o /tmp/${bundle}
 
-aws elasticbeanstalk create-application-version \
-    --application-name ${application} \
-    --version-label ${tag}  \
-    --source-bundle S3Bucket="${bucket}",S3Key="${bundle}" \
-    --no-auto-create-application
+    aws s3 cp /tmp/${bundle} s3://${bucket}
+
+    aws elasticbeanstalk create-application-version \
+        --application-name ${application} \
+        --version-label ${tag}  \
+        --source-bundle S3Bucket="${bucket}",S3Key="${bundle}" \
+        --no-auto-create-application
+else
+    echo "Version ${tag} already uploaded."
+fi
